d5328c86b36d05dabc9131c79dfae3459bcc6c83
Daniele Basile <asterix@develer.com>
2010-03-21 22:59:55 +0100

    Added preference class and window.

---
+++ preference.cpp
@@ -0,0 +1,51 @@
+/**
+ * \file
+ * <!--
+ * Tweezers is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Copyright 2010 Daniele Basile <asterix@develer.com>
+ *
+ * -->
+ *
+ * \brief Tweezers preference window.
+ *
+ * \author Daniele Basile <asterix@develer.com>
+ */
+#include "preference.h"
+#include "ui_preference.h"
+
+Preference::Preference(QWidget *parent) :
+    QWidget(parent),
+    m_ui(new Ui::Preference)
+{
+    m_ui->setupUi(this);
+}
+
+Preference::~Preference()
+{
+    delete m_ui;
+}
+
+void Preference::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        m_ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ preference.h
@@ -0,0 +1,48 @@
+/**
+ * \file
+ * <!--
+ * Tweezers is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Copyright 2010 Daniele Basile <asterix@develer.com>
+ *
+ * -->
+ *
+ * \brief Tweezers preference window.
+ *
+ * \author Daniele Basile <asterix@develer.com>
+ */
+#ifndef PREFERENCE_H
+#define PREFERENCE_H
+
+#include <QtGui/QWidget>
+
+namespace Ui {
+    class Preference;
+}
+
+class Preference : public QWidget {
+    Q_OBJECT
+public:
+    Preference(QWidget *parent = 0);
+    ~Preference();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::Preference *m_ui;
+};
+
+#endif // PREFERENCE_H
