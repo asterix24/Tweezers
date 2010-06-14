0109e901a88d2a915c57f0302e064171c9170edf
Daniele Basile <asterix@panoramix.local>
2010-06-14 23:40:58 +0200

    Fix home path.

--- build_tools.sh
+++ build_tools.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd tools/exiv2
-./configure --enable-shared=no --enable-static=yes --prefix=/home/asterix/src/tweezers_cpp/tools/exiv2/../../lib/
+./configure --enable-shared=no --enable-static=yes --prefix=$HOME/src/tweezers_cpp/tools/exiv2/../../lib/
 
 make
 make install
